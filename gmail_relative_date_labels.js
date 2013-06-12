0b01550dd4c92452a1577ecf5bc5bc6de0fa95ac
Geoffrey Arnold <geoffrey@geoffreyarnold.com>
2013-06-12 16:52:17 -0400

    Modified extension to use "[newer|older]_than" search operators to simplify logic (https://support.google.com/mail/answer/7190?hl=en).

--- gmail_relative_date_labels.js
+++ gmail_relative_date_labels.js
@@ -5,11 +5,11 @@
         if (composeButtonNavigationDiv) {
             var div = document.createElement('div');
             div.id = 'gmail_relative_date_labels';
-            div.appendChild(createAnchor('Today', 0, 1));
-            div.appendChild(createAnchor('Yesterday', -1, 0));
-            div.appendChild(createAnchor('Last 7 days', -7, 1));
-            div.appendChild(createAnchor('Last 30 days', -30, 1));
-            div.appendChild(createAnchor('Last 365 days', -365, 1));
+            div.appendChild(createAnchor('Today', '1d', null));
+            div.appendChild(createAnchor('Yesterday', '2d', '1d'));
+            div.appendChild(createAnchor('Past week', '7d', null));
+            div.appendChild(createAnchor('Past month', '1m', null));
+            div.appendChild(createAnchor('Past year', '1y', null));
 
             composeButtonNavigationDiv.insertBefore(div, composeButtonNavigationDiv.firstChild.nextSibling);
         }
@@ -32,7 +32,7 @@
         return false;
     }
 
-    function createAnchor(name, afterOffset, beforeOffset) {
+    function createAnchor(name, newerThanOffset, olderThanOffset) {
         var div = document.createElement('div');
         div.innerHTML = '<div class="aim"><div class="TN"><div class="aio"><a href="#" class="J-Ke n0">' + name + '</a></div></div></div>';
         div.onclick = function () {
@@ -43,31 +43,31 @@
 
             this.querySelector('.TN').className = 'TN active_filter';
 
-            applyFilter(afterOffset, beforeOffset);
+            applyFilter(newerThanOffset, olderThanOffset);
         };
 
         return div;
     }
 
-    function applyFilter(afterOffset, beforeOffset) {
+    function applyFilter(newerThanOffset, olderThanOffset) {
         var searchInput = document.getElementById('gbqfq');
         var searchButton = document.getElementById('gbqfb');
         if (searchInput && searchButton) {
-            var after = 'after:' + formatDate(createDate(afterOffset));
-            var before = beforeOffset > 0 ? '' : 'before:' + formatDate(createDate(beforeOffset));
+            var newerThan = 'newer_than:' + newerThanOffset;
+            var olderThan = olderThanOffset ? 'older_than:' + olderThanOffset : '';
 
             var query = searchInput.value;
-            if (query.match(/ ?after:.+ ?/)) {
-                query = query.replace(/after:[^ ]+/, after);
+            if (query.match(/ ?newer_than:.+ ?/)) {
+                query = query.replace(/newer_than:[^ ]+/, newerThan);
             }
             else {
-                query += ' ' + after;
+                query += ' ' + newerThan;
             }
-            if (query.match(/ ?before:.+ ?/)) {
-                query = query.replace(/before:[^ ]+/, before);
+            if (query.match(/ ?older_than:.+ ?/)) {
+                query = query.replace(/older_than:[^ ]+/, olderThan);
             }
             else {
-                query += ' ' + before;
+                query += ' ' + olderThan;
             }
 
             searchInput.value = query;
@@ -75,20 +75,8 @@
         }
     }
 
-    function createDate(dateOffset) {
-        var date = new Date();
-        date.setDate(date.getDate() + dateOffset);
-
-        return date;
-    }
-
-    function formatDate(date) {
-        return date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate();
-    }
-
     window.setTimeout(function () {
         addLabelsIfReady();
     }, 250);
-
 })();
 
